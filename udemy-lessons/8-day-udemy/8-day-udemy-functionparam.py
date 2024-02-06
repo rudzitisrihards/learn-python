e1a69090b15f0bc4501e20d4b64e40d3f05dafc1
Rihards <rudzitis.rihards@gmail.com>
2024-02-06 18:52:00 +0200

    day 8

---
+++ udemy-lessons/8-day-udemy/8-day-udemy-functionparam.py
@@ -0,0 +1,16 @@
+def greet():
+    print("line1")
+    print("line2")
+    print("line3")
+
+greet()
+
+def greet_person(name):
+    print(f"Hello, {name}")
+
+greet_person("Rihards")
+
+def greet_with(name, location):
+    print(f"Hello, {name}! How is the weather in {location}?")
+
+greet_with("Rihards", "Riga")
