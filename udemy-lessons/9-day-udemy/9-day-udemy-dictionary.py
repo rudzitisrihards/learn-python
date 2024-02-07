999cc5bf4573c0cccd0a00066670af2a36e61bc4
Rihards <rudzitis.rihards@gmail.com>
2024-02-07 17:16:41 +0200

    day 9 and 15

---
+++ udemy-lessons/9-day-udemy/9-day-udemy-dictionary.py
@@ -0,0 +1,11 @@
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.", 
+    "Function": "A piece of code that you can easily call over and over again.",
+}
+
+travel_log = {
+    "France": ["Paris", "Lille", "Toulouse"],
+    "Germany": ["Berlin", "Hamburg", "Stuttgart"]
+}
+
+print(programming_dictionary["Function"])
